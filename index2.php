069c9abc727a4696fd2712bef098f822f4eb3cd4
tapootum <tapootumchannel@gmail.com>
2011-11-25 14:05:09 +0700

    chap1 OK

--- index2.php
+++ index2.php
@@ -2,22 +2,22 @@
 	$con = mysql_connect("localhost","root","kawaoisoki");
 		if (!$con)
   		{  die('Could not connect: ' . mysql_error()); }
-#	$user = $_POST["user"];
-#	$group = $_POST["group"];
+	$user = $_POST["user"];
+	$group = $_POST["group"];
 	
 mysql_select_db("mail", $con);
 	
 
 	$sql = "select address.*".
 		" from address,addressgroups ".
-		"where address.owner = "test1" ".
+		"where address.owner = '$user' ".
 		"and addressgroups.nickname = address.nickname ".
-		"and addressgroups.addressgroup = "test1x"";
+		"and addressgroups.addressgroup = '$group'";
 	$result=mysql_query($sql);
 
 	while($row = mysql_fetch_array($result))
  	 {
-  	echo $row['owner'] . " " . 
+  	echo  
 		$row['nickname'] . " " . 
 		$row['firstname'] . " " . 
 		$row['lastname'] . " " . 
