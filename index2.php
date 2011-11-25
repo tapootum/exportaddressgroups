ec20544a7d23f88527806ffb22dbdf66a7e6e73b
tapootum <tapootumchannel@gmail.com>
2011-11-25 13:52:07 +0700

    index2 aa

--- index2.php
+++ index2.php
@@ -1,23 +1,28 @@
 <?php
-	$con = mysql_connect("localhost","root","123456789");
+	$con = mysql_connect("localhost","root","kawaoisoki");
 		if (!$con)
   		{  die('Could not connect: ' . mysql_error()); }
-	$user = $_POST["user"];
-	#$group = $_POST["group"];
-
+#	$user = $_POST["user"];
+#	$group = $_POST["group"];
+	
+mysql_select_db("mail", $con);
 	
 
-
-#################################################
-	mysql_select_db("mail", $con);
-	
-
-	$sql = "select address.* from address,addressgroups where addressgroups.owner = $user and (addressgroups.nickname = address.nickname = $user;
+	$sql = "select address.*".
+		" from address,addressgroups ".
+		"where address.owner = "test1" ".
+		"and addressgroups.nickname = address.nickname ".
+		"and addressgroups.addressgroup = "test1x"";
 	$result=mysql_query($sql);
 
 	while($row = mysql_fetch_array($result))
  	 {
-  	echo $row['FirstName'] . " " . $row['LastName'];
+  	echo $row['owner'] . " " . 
+		$row['nickname'] . " " . 
+		$row['firstname'] . " " . 
+		$row['lastname'] . " " . 
+		$row['email'] . " " . 
+		$row['label'];
   	echo "<br />";
   	}
 
