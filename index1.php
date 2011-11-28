63fdf910c50e705bfcb8acc5209a605d68a22aaa
tapootum <tapootumchannel@gmail.com>
2011-11-28 12:32:50 +0700

    Grean Complete........

--- index1.php
+++ index1.php
@@ -14,24 +14,24 @@ $objQuery = mysql_query($strSQL) or die ("Error Query [".$strSQL."]");
 ?>
 <table width="600" border="1">
   <tr>
-    <th width="91"> <div align="center">Nickname </div></th>
-    <th width="98"> <div align="center">FirstName </div></th>
-    <th width="198"> <div align="center">Lastname </div></th>
-    <th width="97"> <div align="center">E-mail </div></th>
-    <th width="59"> <div align="center">Label </div></th>
-    <th width="71"> <div align="center">Group </div></th>
+    <th> <div align="center">Nickname </div></th>
+    <th> <div align="center">FirstName </div></th>
+    <th> <div align="center">Lastname </div></th>
+    <th> <div align="center">E-mail </div></th>
+    <th> <div align="center">Label </div></th>
+    <th> <div align="center">Group </div></th>
   </tr>
 <?
 while($objResult = mysql_fetch_array($objQuery))
 {
 ?>
   <tr>
-    <td><div align="center"><?=$objResult["nickname"];?></div></td>
-    <td><?=$objResult["firstname"];?></td>
-    <td><?=$objResult["lastname"];?></td>
-    <td><div align="center"><?=$objResult["email"];?></div></td>
-    <td align="right"><?=$objResult["label"];?></td>
-    <td align="right"><?=$objResult["addressgroup"];?></td>
+    <td align="center"><?=$objResult["nickname"];?></div></td>
+    <td align="center"><?=$objResult["firstname"];?></td>
+    <td align="center"><?=$objResult["lastname"];?></td>
+    <td align="center"><?=$objResult["email"];?></div></td>
+    <td align="center"><?=$objResult["label"];?></td>
+    <td align="center"><?=$objResult["addressgroup"];?></td>
   </tr>
 
 <?
@@ -53,12 +53,12 @@ while($objResult = mysql_fetch_array($objQuery))
 $array[$i]=$objResult["addressgroup"];
 $i=$i+1;
 }
-
-for ($j=0;$j<=$i;$j=$j+1){
-echo $array[$j];
+/*
+for ($j=0;$j<$i;$j=$j+1){
+echo "   gruop ".$array[$j];
 }
 
-
+*/
 ?>
 
 
@@ -76,10 +76,10 @@ mysql_close($objConnect);
 
 
 
-for ($j=0;$j<=$i;$j++){
+for ($j=0;$j<$i;$j++){
 
 	$filName = "user-".$user."-gruop-".$array[$j].".csv";
-	$path = "/csv/".$filName;
+	$path = "csv/".$filName;
 	$objWrite = fopen($path, "w+");
 
 	$objConnect = mysql_connect("localhost","root","kawaoisoki") or die("Error Connect to Database");
@@ -100,7 +100,7 @@ for ($j=0;$j<=$i;$j++){
 	fclose($objWrite);
 	echo "<br>Generate ";
 	echo $filName;
-	echo " Done.<br><a href=$path>Download</a>";
+	echo " Done.  <a href=$path>Download</a>";
 
 }
 
